3b36ca5460c74f3b3a8c4c0b469221d88b67ea8f
azaa887777 <azaa88777@gmail.com>
2023-07-09 00:08:24 +0100

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,37 +1,70 @@
 import fetch from 'node-fetch'
+import axios from 'axios'
+import cheerio from 'cheerio'
 import instagramGetUrl from 'instagram-url-direct'
+import vm from 'node:vm'
+import { instagram } from "@xct007/frieren-scraper";
 import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, command, usedPrefix }) => {
-if (!args[0]) throw `*[instagram video downloader]* 
+if (!args[0]) throw `
 
-هذا الامر خاص بتحميل فيديوات الانستغرام 
-
-exemple: *${usedPrefix + command}* https://www.instagram.com/reel/CmWUIDXuB_J/?igshid=Yzg5MTU1MDY=` 
+   Had cmd khasss b Instagram :)
+   Dir lia follow b3da hna?
+   www.instagram.com/mh7__x
+   
+*𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command}* *https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link*` 
+m.reply(`*Sbr ashbi mlk ax tab3k?*
+Aji dir lia follow hna b3da :) >> https://www.instagram.com/mh7__x`)
 try {
-await m.reply(`*stana xwia, yak ba3da mazarbanx :)*`)
-switch (command) {        
-case "instagramdl2": case "instagram2": case "igdl2": case "ig2": 
+const datTa = await instagram.v1(args[0])
+if (datTa.error) return m.reply(`${datTa.message}`);
+for (let urRRl of datTa) {
+let shortUrRRl = await (await fetch(`https://tinyurl.com/api-create.php?url=${urRRl.url}`)).text()    
+let tXXxt = `🔗 *Url:* ${shortUrRRl}`.trim()    
+conn.sendFile(m.chat, urRRl.url, 'error.mp4', tXXxt, m)    
+await new Promise(resolve => setTimeout(resolve, 10000))}
+} catch {  
+try {  
+const AA = await savefrom(args[0])    
+let shortUrl222 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
+let txt222 = `🔗 *Url:* ${shortUrl222}`.trim()
+await conn.sendFile(m.chat, AA.url[0].url, 'error.mp4', txt222, m)
+} catch {
+console.log('Error en el intento 1')  
+try {    
+const resultss = await instagramGetUrl(args[0]).url_list[0]    
+let shortUrl2 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
+let txt2 = `🔗 *Url:* ${shortUrl2}`.trim()
+await conn.sendFile(m.chat, resultss, 'error.mp4', txt2, m)
+} catch {  
+console.log('Error en el intento 2')  
+try {
+const resultssss = await instagramdl(args[0]).catch(async _ => await instagramdlv2(args[0])).catch(async _ => await instagramdlv3(args[0])).catch(async _ => await instagramdlv4(args[0]))
+let shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${r.medias[i].url}`)).text()
+let txt4 = `🔗 *Url:* ${shortUrl3}`.trim()
+for (const { url } of resultssss) await conn.sendFile(m.chat, url, 'error.mp4', txt4, m)
+} catch {
+console.log('Error en el intento 3')  
+try {   
 let human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=${lolkeysapi}&url=${args[0]}`)
 let json = await human.json()
 let videoig = json.result
 let shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
 let txt1 = `🔗 *Url:* ${shortUrl1}`.trim()
-await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m)
-break  
-case "instagramdl": case "instagram": case "igdl": case "ig":        
-const resultss = (await instagramGetUrl(args[0])).url_list[0]
-let shortUrl2 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
-let txt2 = `🔗 *Url:* ${shortUrl2}`.trim()
-conn.sendFile(m.chat, resultss, 'error.mp4', txt2, m)
-break               
-case "instagramdl3": case "instagram3": case "igdl3": case "ig3":            
-const resultssss = await instagramdl(args[0]).catch(async _ => await instagramdlv2(args[0])).catch(async _ => await instagramdlv3(args[0])).catch(async _ => await instagramdlv4(args[0]))
-let shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${r.medias[i].url}`)).text()
-let txt3 = `🔗 *Url:* ${shortUrl3}`.trim()
-for (const { url } of resultssss) await conn.sendFile(m.chat, url, 'error.mp4', txt4, m)
-break       
-}} catch {
-await await m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁,  (${usedPrefix}ig, ${usedPrefix}ig2, ${usedPrefix}ig3)*`)
-}}
-handler.command = /^(instagramdl2|instagram2|igdl2|ig2|instagramdl|instagram|igdl|ig|instagramdl3|instagram3|igdl3|ig3)$/i
+await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m)     
+} catch { 
+console.log('Error en el intento 4, sin resultados')  
+throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*`
+}}}}}}
+handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i
 export default handler
+async function savefrom(urlL) {
+let body = new URLSearchParams({ "sf_url": encodeURI(urlL), "sf_submit": "", "new": 2, "lang": "id", "app": "", "country": "id", "os": "Windows", "browser": "Chrome", "channel": " main", "sf-nomad": 1 });
+let { data } = await axios({ "url": "https://worker.sf-tools.com/savefrom.php", "method": "POST", "data": body, "headers": { "content-type": "application/x-www-form-urlencoded", "origin": "https://id.savefrom.net", "referer": "https://id.savefrom.net/", "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36" }});
+let exec = '[]["filter"]["constructor"](b).call(a);';
+data = data.replace(exec, `\ntry {\ni++;\nif (i === 2) scriptResult = ${exec.split(".call")[0]}.toString();\nelse (\n${exec.replace(/;/, "")}\n);\n} catch {}`);
+let context = { "scriptResult": "", "i": 0 };
+vm.createContext(context);
+new vm.Script(data).runInContext(context);
+return JSON.parse(context.scriptResult.split("window.parent.sf.videoResult.show(")?.[1].split(");")?.[0])
+}
